5ef9920dab9913f72a9a9d234b4a22c840d87905
Yang Wang <yang.wang.dec@gmail.com>
2020-04-12 22:12:00 -0400

    image url to abs path

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// setTimeout(() => {
+let autoScroll = false;
+
 $(function () {
     let number = 3000;
     let pixel = 500;
     setInterval(() => {
-        $("html, body").animate({ scrollTop: pixel }, 3000);
-        pixel += 500;
+    	if(autoScroll){;
+        	$("html, body").animate({ scrollTop: pixel }, 3000);
+        	pixel += 500;
+    	}
     }, 10000);
 });
 
+
+function toggleAS(){
+	autoScroll=!autoScroll;
+	console.log("auto scroll now is "+autoScroll);
+	if(autoScroll)
+	{
+				$("body").css("background","rgb(230, 230, 230)");
+		$("#auto-scroll-button").html("turn off auto scroll");
+	}else{
+		$("body").css("background","rgb(255,255,255)");
+		$("#auto-scroll-button").html("turn on auto scroll");
+	}
+}
 
